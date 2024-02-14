d3cc59de053af1d9bda4a3cd3f1366ff9da51e96
Gangmin Kim <kkmin0713@naver.com>
2024-02-14 11:03:31 +0900

    second commit

--- main.js
+++ main.js
@@ -5,8 +5,12 @@ let TaskList = []
 AddButton.addEventListener("click",addTask)
 
 function addTask(){
-    let TaskValue = TaskInput.value
-    TaskList.push(TaskValue)
+    let task = {
+        id: newID(),
+        TaskContent: TaskInput.value,
+        isComplete:false
+    }
+    TaskList.push(task)
     console.log(TaskList)
     render()
 }
@@ -14,13 +18,48 @@ function addTask(){
 function render(){
     let resultHTML = ``
     for(let i=0;i<TaskList.length;i++){
+      if(TaskList[i].isComplete==true){
         resultHTML += `<div class="task">
-        <div>${TaskList[i]}</div>
+        <div class="task-done">${TaskList[i].TaskContent}</div>
         <div>
-            <button>Check</button>
-            <button>Delete</button>
+            <button onclick="toggleComplete('${TaskList[i].id}')"><i class="fa-solid fa-rotate-left"></i></button>
+            <button onclick="deleteTask('${TaskList[i].id}')"><i class="fa-solid fa-trash-can"></i></button>
         </div>
-    </div>`;
+    </div>`
+      }else{
+        resultHTML += `<div class="task">
+        <div>${TaskList[i].TaskContent}</div>
+        <div>
+            <button onclick="toggleComplete('${TaskList[i].id}')"><i class="fa-solid fa-check"></i></button>
+            <button onclick="deleteTask('${TaskList[i].id}')"><i class="fa-solid fa-trash-can"></i></button>
+        </div>
+    </div>`
+      }
     }
     document.getElementById("task-board").innerHTML = resultHTML
 }
+
+function toggleComplete(id){
+    console.log("id:",id)
+    for(let i=0;i<TaskList.length;i++){
+        if(TaskList[i].id==id){
+            TaskList[i].isComplete = !TaskList[i].isComplete
+            break
+        }
+    }
+    render()
+    console.log(TaskList)
+}
+
+function deleteTask(id){
+    for(let i=0;i<TaskList.length;i++){
+        if(TaskList[i].id==id){
+            TaskList.splice(i,1)
+            break
+        }
+    }
+    render()
+}
+
+function newID(){
+    return Math.random().toString(36).substr(2, 16)}
